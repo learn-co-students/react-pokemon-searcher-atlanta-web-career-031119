d9acb9ed35ce85d58947a88b2eab4a81f2084ffd
PrincetonJRose <decoyzpr@gmail.com>
2019-05-17 09:43:19 -0400

    Finished everything except adding in the debouncer.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
+import '../index.css'
 
 class PokemonCard extends React.Component {
+  constructor() {
+    super()
+    this.state = { picture: true }
+  }
+
+  changeFacing =()=> {
+    this.setState({ picture: !this.state.picture })
+  }
+
   render() {
     return (
-      <Card>
+      <Card className="pokemon-card">
         <div>
-          <div className="image">
-            <img alt="oh no!" />
+          <div className="image" onClick={this.changeFacing}>
+          {this.state.picture ? 
+            <img src={this.props.pokemon.sprites.front} alt="oh no!" /> 
+            : <img src={this.props.pokemon.sprites.back}/>}
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemon.name.charAt(0).toUpperCase() + this.props.pokemon.name.slice(1)}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              HP: {this.props.pokemon.stats.map( stats => {
+                if (stats.name === "hp") {
+                  return stats.value
+                }})}
             </span>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import 'semantic-ui-css/semantic.min.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
